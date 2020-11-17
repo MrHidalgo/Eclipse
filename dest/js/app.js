f66d03567e2c843c7e566229457232f4ea2140ea
Andrii <Luckyhidalgo@gmail.com>
2020-11-17 11:07:14 +0200

    Setup section - fixed video preview - w\o adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -70,6 +70,28 @@ var initPopups = function initPopups() {
 	$('[popup-video-js]').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
+		iframe: {
+			patterns: {
+				youtube: {
+					index: 'youtube.com/',
+					id: function id(url) {
+						var m = url.match(/[\\?\\&]v=([^\\?\\&]+)/);
+						if (!m || !m[1]) return null;
+						return m[1];
+					},
+					src: '//www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: function id(url) {
+						var m = url.match(/(https?:\/\/)?(www.)?(player.)?vimeo.com\/([a-z]*\/)*([0-9]{6,11})[?]?.*/);
+						if (!m || !m[5]) return null;
+						return m[5];
+					},
+					src: '//player.vimeo.com/video/%id%?autoplay=1'
+				}
+			}
+		},
 		mainClass: 'is-show',
 		removalDelay: 300,
 		preloader: false,
